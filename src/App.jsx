dbc86e91f5a510c7f8d7c595ca1a195504f5d962
mrcll <marcell.csokas@pm.me>
2020-08-13 14:06:55 +0200

    finished with the functionality, I'll continue with styling

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Lucky from "./Lucky";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Lotto 6/49</h1>
+        <h3>Generating lucky numbers</h3>
+        <Lucky />
+      </div>
+    );
+  }
+}
+
+export default App;
